230dcf24de82d7e737b1a67ceb8dca1114c0b0f4
Cristian Mellibosky <111097@tecnicatura.frc.utn.edu.ar>
2021-03-31 16:06:42 -0300

    contenido sobre JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const user = {
+  firstName: "Cristian",
+  lastName: "Mellibosky",
+  avatar:"https://i.imgur.com/i1tmH.png"
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function getName(user){
+  return `${user.firstName} ${user.lastName}`
+}
+
+function getGreeting(user){
+  if(user){
+    return  <h1>Hola {getName(user)}</h1>
+  }
+  return <h1>Hola Stranger</h1>
+}
+const name = 'Cristian'
+
+const element = (
+<div>
+  {getGreeting(user)}
+  <img src={user.avatar}/>  
+</div>
+)
+
+const container = document.getElementById("root")
+
+ReactDOM.render(element, container)
